552a5d1e21516044dd3957b871c7b570d57b80d6
sarath.krishnan@slalom.com-at-617780835636 <sarath.krishnan@S01423-MBPR.local>
2020-04-28 03:29:47 -0400

    Initial commit

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ async function createConfigVersion(options) {
         const body = { data: { type: "configuration-versions", attributes: { "auto-queue-runs": false } } };
         const terraformConfigVersionsEndpoint = "https://" + terraformHost + "/api/v2/workspaces/" + workSpaceId + "/configuration-versions";
         console.log("terraformConfigVersionsEndpoint:"+terraformConfigVersionsEndpoint);
-        res = await axios.post(terraformConfigVersionsEndpoint, body, options);
+        res = await axios.post(terraformConfigVersionsEndpoint, JSON.stringify(body), options);
         const configVersion = res.data.data;
         console.log("create config version response:"+ res.data.data);
         return { id: configVersion.id, uploadUrl: configVersion.attributes['upload-url'] };
